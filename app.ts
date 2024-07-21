12e53915165f2c3f9b9fdb2ac3a9e310040b3091
Juoelenis <116748990+Juoelenis@users.noreply.github.com>
2024-07-21 23:51:26 +0300

    HPP files
    These are essential files in hpp

---
+++ app.ts
@@ -0,0 +1,12 @@
+let message: string = 'Deez Nuts';
+console.log(message)
+
+//creates a chat log
+let heading = document .createElement ('h1')
+heading.textContent = message;
+
+
+function greeeter(person){
+
+    return"hello"
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('hello world');
+for (let i = 0;  i < 11; i++)  {
+if (i % 2 ==0) continue;
+   console.log
+ frames 
+ ImageBitmap 
+ function newFunction();
+     Gamepad =  false;
+ } while (condition);
+
+function newFunction() {
+    var ;
+}
